07e24d9d17d5a64cf6ca7823aecdd28bfc5f704c
Gurpreet266 <simrankaurmaan.sk@gmail.com>
2025-11-10 10:55:34 +0530

    made changes to product package an added organization package

---
+++ src/test/java/product/Product_Test.java
@@ -0,0 +1,13 @@
+package product;
+
+import org.testng.annotations.Test;
+
+public class Product_Test {
+	@Test
+	public void addProduct()
+	{
+		System.out.println("---Test 1--");
+		System.out.println("--Test 2--");
+	}
+
+}
--- src/test/java/product/addProduct_Test.java
+++
@@ -1,8 +0,0 @@
-package product;
-
-public class addProduct_Test {
-	public static void main(String[]args) {
-		System.out.println("---Test 1--");
-	}
-
-}
